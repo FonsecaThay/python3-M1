576125dabd4732f0a135d55b00bbfcc5e9d79d40
Thaynara Fonseca <tatafonseca15@gmail.com>
2022-08-01 23:46:23 -0300

    Create desafio 005.py

---
+++ desafios/desafio 005.py
@@ -0,0 +1,7 @@
+a = int(input('Digite um número:'))
+s = a + 1
+an = a - 1
+print(f'O sucessor desse número é {s}')
+print(f"O antecessor desse número é {an}")
+#ou
+#print(f'o antecessor é {a-1} e o sucessor é {a+1}')
