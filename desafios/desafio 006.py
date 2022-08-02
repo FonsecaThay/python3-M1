70a9846c65608b9c165ad6a592e3b04426bcf461
Thaynara Fonseca <tatafonseca15@gmail.com>
2022-08-01 23:46:31 -0300

    Create desafio 006.py

---
+++ desafios/desafio 006.py
@@ -0,0 +1,9 @@
+import math
+
+a = float(input('Digite um número:'))
+
+d = a * 2
+t = a * 3
+r = math.pow(a,1/2)
+#print(f'O dobro de {a} é:{a * 2};\no triplo de {a} é: {a * 3} ; \na raiz quadrada de {a} é:{a ** (1/2)}')
+print(f'O dobro de {a} é:{d};\no triplo de {a} é: {t} ; \na raiz quadrada de {a} é: {r}')
