6dd153b3c7b3533221216c24542937be44f0f20e
Thaynara Fonseca <tatafonseca15@gmail.com>
2022-08-01 23:46:40 -0300

    Create desafio 008.py

---
+++ desafios/desafio 008.py
@@ -0,0 +1,4 @@
+v = float(input('Digite uma distância metros:'))
+print(f'A distância {v} em centímetros é {v * 100};\nA distância {v} em milímetros é: {v * 1000}')
+print(f'A distância {v} em decímetros é {v * 10};\nA distância {v} em decâmetros é {v / 10} ')
+print(f'A distância {v} em hectometros é {v / 100};\nA distância {v} em quilômetros é {v / 1000} ')
