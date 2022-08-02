a296f33c02cccc7d1dd907aae17d74a9dd4636bd
Thaynara Fonseca <tatafonseca15@gmail.com>
2022-08-01 23:46:51 -0300

    Create desafio 011.py

---
+++ desafios/desafio 011.py
@@ -0,0 +1,7 @@
+la = float(input('Qual é a largura da parede:'))
+alt = float(input('Qual é a altura da parede:'))
+a = la * alt
+print(f'Sua parede tem a dimensão de {la} x {alt} e sua área é de {a}m2')
+tinta = a / 2
+print(f'A quantidade de tinta que vc vai precisar é de:{tinta}litros.')
+
