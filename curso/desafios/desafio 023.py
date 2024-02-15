c8fa0e8bd36763ecbbeb744f9f0df0d3c39c3cdf
Thaynara Fonseca <tatafonseca15@gmail.com>
2024-02-15 12:26:21 -0300

    Update desafio 023.py

--- curso/desafios/desafio 023.py
+++ curso/desafios/desafio 023.py
@@ -4,4 +4,15 @@ ex: Digite um numero:1834
 unidade:4
 dezena :3
 centena:8
-milhar:1""")
+milhar:1\n""")
+
+num = int(input("Digite um número: "))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print("Analisando o numero {}".format(num))
+print("Unidade:{}".format(u))
+print("Dezena:{}".format(d))
+print("Centena:{}".format(c))
+print("Milhar:{}".format(m))
