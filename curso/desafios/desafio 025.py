9d749328ee3605b42bc4570930e048bc0e2b6a68
Thaynara Fonseca <tatafonseca15@gmail.com>
2024-02-15 12:43:09 -0300

    Update desafio 025.py

--- curso/desafios/desafio 025.py
+++ curso/desafios/desafio 025.py
@@ -1,2 +1,5 @@
 print("""crie um programa que leia o nome de uma pessoa
 e diga se ela tem "SILVA" no nome""")
+
+nome = str(input("Digite seu nome: "))
+print("Seu nome tem Silva? {}".format("SILVA" in nome.upper()))
