ddb0f76cc4387f57621d77b3c41306604c49006e
Thaynara Fonseca <tatafonseca15@gmail.com>
2024-02-15 12:34:47 -0300

    Update desafio 024.py

--- curso/desafios/desafio 024.py
+++ curso/desafios/desafio 024.py
@@ -1,2 +1,5 @@
 print("""Crie um programa que leia o nome de uma 
 cidade e diga se ela começa ou não com o nome "Santo""")
+
+cidade = str(input("Digite a cidade em que você nasceu: ")).strip()
+print (cidade[:5].upper() == "SANTO")
